aa7531416248f1ae0385ae004850ea01a911c814
arition <aritionkb@gmail.com>
2016-09-09 16:41:25 +0800

    heartBeatClient WIP

---
+++ src/HeartBeatClient/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.CommandLine;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+namespace HeartBeatClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string username = null;
+            string password = null;
+            string tokenEndPoint = null;
+            string heartBeatEndPoint = null;
+            string device = null;
+            string configFile = null;
+
+            ArgumentSyntax.Parse(args, t =>
+            {
+                t.DefineOption("un|username", ref username, "Login Username");
+                t.DefineOption("pw|password", ref password, "Login Password");
+                t.DefineOption("turl|tokenEndPoint", ref tokenEndPoint, "Token EndPoint");
+                t.DefineOption("hurl|tokenEndPoint", ref heartBeatEndPoint, "HeartBeat EndPoint");
+                t.DefineOption("dev|device", ref device, "DeviceName");
+                t.DefineOption("c|config", ref configFile,
+                    "Config File. The option in file will overwrite options provided by args.");
+
+                if (string.IsNullOrEmpty(configFile))
+                {
+                    if (string.IsNullOrEmpty(username)) throw new ArgumentSyntaxException("username is required");
+                    if (string.IsNullOrEmpty(password)) throw new ArgumentSyntaxException("password is required");
+                    if (string.IsNullOrEmpty(device)) throw new ArgumentSyntaxException("device is required");
+                    if (string.IsNullOrEmpty(tokenEndPoint))
+                        throw new ArgumentSyntaxException("tokenEndPoint is required");
+                    if (string.IsNullOrEmpty(heartBeatEndPoint))
+                        throw new ArgumentSyntaxException("heartBeatEndPoint is required");
+                }
+            });
+
+            if (!string.IsNullOrEmpty(configFile))
+            {
+                using (var jsonText = File.OpenText(configFile))
+                {
+                    var json = JObject.Parse(jsonText.ReadToEnd());
+                    username = json["username"].ToString();
+                    password = json["password"].ToString();
+                    password = json["device"].ToString();
+                    tokenEndPoint = json["token_end_point"].ToString();
+                    heartBeatEndPoint = json["heart_beat_end_point"].ToString();
+                }
+            }
+
+            var token = new Token(tokenEndPoint, username, password);
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", token.GetAccessToken().Result);
+
+            while (true)
+            {
+                try
+                {
+                    var response =
+                        httpClient.PostAsync(heartBeatEndPoint, new StringContent("{\"device\":\"" + device + "\"}")).Result;
+                    if (response.StatusCode == HttpStatusCode.Unauthorized)
+                    {
+                        httpClient.DefaultRequestHeaders.Authorization =
+                            new AuthenticationHeaderValue("Bearer", token.GetAccessToken().Result);
+                    }
+                    Task.Delay(10*1000).Wait();
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ src/HeartBeatClient/Token.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace HeartBeatClient
+{
+    public class Token:IDisposable
+    {
+        public string TokenEndPoint { get; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string RefreshToken { get; private set; }
+
+        private HttpClient HttpClient { get; } = new HttpClient();
+
+        public Token(string tokenEndPoint,string username,string password)
+        {
+            TokenEndPoint = tokenEndPoint;
+            Username = username;
+            Password = password;
+        }
+
+        public async Task<string> GetAccessToken()
+        {
+            var response =
+                await HttpClient.PostAsync(TokenEndPoint, new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    {"username", Username},
+                    {"password", Password},
+                    {"grant_type", "password"},
+                    {"scope", "roles offline_access"}
+                }));
+            response.EnsureSuccessStatusCode();
+            var json = JObject.Parse(await response.Content.ReadAsStringAsync());
+            RefreshToken = json["refresh_token"].ToString();
+            return json["access_token"].ToString();
+        }
+
+        public async Task<string> GetAccessToken(bool useRefreshToken)
+        {
+            if (!useRefreshToken) return await GetAccessToken();
+            if (string.IsNullOrEmpty(RefreshToken)) throw new ArgumentNullException(nameof(RefreshToken));
+            var response =
+                await HttpClient.PostAsync(TokenEndPoint, new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    {"grant_type", "refresh_token"},
+                    {"refresh_token", RefreshToken}
+                }));
+            response.EnsureSuccessStatusCode();
+            var json = JObject.Parse(await response.Content.ReadAsStringAsync());
+            RefreshToken = json["refresh_token"].ToString();
+            return json["access_token"].ToString();
+        }
+
+        public void Dispose()
+        {
+            HttpClient.Dispose();
+        }
+    }
+}
